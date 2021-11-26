ea4c58953cbf51404a7dd7719e896a3bae69d2e7
darcy <darcy.u83@gmail.com>
2021-11-26 17:38:01 +0900

    typescript  /w style-components theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components'
-import Circle from "./Circle"
+
 
 
 function App() {
+  const Container = styled.div`
+  background-color: ${props=>props.theme.bgColor}`;
+  const H1 = styled.h1`
+  color: ${props=>props.theme.textColor}`;
+
   return (
-   <div>
-     <Circle bgColor="teal"/>
-     <Circle bgColor="tomato" borderColor="teal" text="AAA"/>
-   </div>
+   <Container>
+     <H1>Protected</H1>
+   </Container>
   );
 }
 
